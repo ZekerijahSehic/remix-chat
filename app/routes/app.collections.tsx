08969b286497e1369b455dcc2842433725e05a9f
Zekerijah <zekerijah@hulkapps.com>
2024-01-30 20:36:40 +0100

    Create first route and view

---
+++ app/routes/app.collections.tsx
@@ -0,0 +1,17 @@
+import { Card, Layout, Page } from "@shopify/polaris";
+
+const Collections = () => {
+    return (
+        <Page>
+            <Layout>
+                <Layout.Section>
+                    <Card>
+                        <h1>Collections</h1>
+                    </Card>
+                </Layout.Section>
+            </Layout>
+        </Page>
+    )
+}
+
+export default Collections;
